44ef4c6b9b3c278ecd59aad5f37d5db3981cf569
Goldman <goldmanmine@mail.ru>
2022-12-14 15:26:01 +0300

    Интерактивный чек

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import csv
 
-from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from PyQt5 import uic
 
@@ -10,32 +9,28 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.display_results()
-        self.le_search.textChanged.connect(self.display_results)
+        self.load_table()
+        self.compute()
+        self.table.itemSelectionChanged.connect(self.compute)
 
-    def display_results(self):
-        substring = self.le_search.text()
-        if len(substring) < 3:
-            substring = ''
+    def load_table(self):
         ans = []
-        with open('titanic.csv', 'r', encoding='utf8') as f:
-            reader = csv.DictReader(f, delimiter=',', quotechar='"')
-            headers = reader.fieldnames
+        with open('price.csv', 'r', encoding='utf8') as f:
+            reader = csv.DictReader(f, delimiter=';', quotechar='"')
             for d in reader:
-                if substring.lower() in d['Name'].lower():
-                    ans.append(tuple(d[i] for i in d))
+                ans.append(tuple([d[i] for i in d] + [0]))
         self.table.setRowCount(len(ans))
-        self.table.setColumnCount(len(ans[0]))
-        self.table.setHorizontalHeaderLabels(headers)
         for i, row in enumerate(ans):
             for j, col in enumerate(row):
                 self.table.setItem(i, j, QTableWidgetItem(str(col)))
         self.table.resizeColumnsToContents()
-        colors = [QColor(255, 0, 0), QColor(0, 255, 0)]
+
+    def compute(self):
+        s = 0
         for i in range(self.table.rowCount()):
-            k = int(self.table.item(i, 5).text())
-            for j in range(self.table.columnCount()):
-                self.table.item(i, j).setBackground(colors[k])
+            p, c = map(int, (self.table.item(i, 1).text(), self.table.item(i, 2).text()))
+            s += p * c
+        self.result.setText(str(s))
 
 
 def except_hook(cls, exception, traceback):
