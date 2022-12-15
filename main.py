3187fa5f4662bf99080b8f4abc433ba833484085
Goldman <goldmanmine@mail.ru>
2022-12-15 22:08:05 +0300

    Дедушка сапёра

--- main.py
+++ main.py
@@ -1,59 +1,75 @@
-import sys
-import sqlite3
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
-from PyQt5 import uic
+import pygame as pg
+from random import randint
 
 
-class Example(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.btns = [self.btn_1, self.btn_2, self.btn_3, self.btn_4, self.btn_5, self.btn_6, self.btn_7, self.btn_8,
-                     self.btn_9, self.btn_10, self.btn_11, self.btn_12, self.btn_13, self.btn_14, self.btn_15,
-                     self.btn_16, self.btn_17, self.btn_18, self.btn_19, self.btn_20, self.btn_21, self.btn_22,
-                     self.btn_23, self.btn_24, self.btn_25, self.btn_26, self.btn_27, self.btn_28, self.btn_29,
-                     self.btn_30, self.btn_31, self.btn_32, self.btn_33]
-        self.get_data()
-        for btn in self.btns:
-            btn.clicked.connect(self.get_data)
+class Board:
+    def __init__(self, w, h):
+        self.w, self.h = w, h
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+        self.board = [[-1] * w for _ in range(h)]
 
-    def get_data(self):
-        try:
-            upper_letter = self.sender().text()
-        except Exception:
-            upper_letter = 'А'
-        lower_letter = upper_letter.lower()
-        connection = sqlite3.connect('films_db.sqlite')
-        cursor = connection.cursor()
-        result = cursor.execute(f'''
-        SELECT id, title, year, genre, duration FROM films
-        WHERE title LIKE "{upper_letter}%" OR title LIKE "{lower_letter}%"
-        ''').fetchall()
-        genres = dict(cursor.execute('''
-        SELECT id, title FROM genres
-        ''').fetchall())
-        self.table.setRowCount(len(result))
-        if result:
-            self.lb_info.setText(f'Нашлось {len(result)} записей')
-            self.table.setColumnCount(len(result[0]))
-        else:
-            self.lb_info.setText(f'Ничего не найдено')
-        for i, row in enumerate(result):
-            for j, col in enumerate(row):
-                if j == 3:
-                    col = genres.get(col)
-                self.table.setItem(i, j, QTableWidgetItem(str(col)))
-        self.table.resizeColumnsToContents()
+    def on_board(self, x, y):
+        return self.w - 1 >= x >= 0 <= y <= self.h - 1
+
+    def draw(self):
+        screen.fill('black')
+        font = pg.font.Font(None, 20)
+        for y in range(self.h):
+            for x in range(self.w):
+                pos = self.board[y][x]
+                if pos == 10:
+                    pg.draw.rect(screen, 'red',
+                                 (self.left + x * self.cell_size, self.top + y * self.cell_size,
+                                  self.cell_size, self.cell_size))
+                elif pos > -1:
+                    text = font.render(f'{pos}', True, (0, 255, 0))
+                    screen.blit(text, (self.left + 2 + x * self.cell_size, self.top + 2 + y * self.cell_size))
+                pg.draw.rect(screen, 'white',
+                             (self.left + x * self.cell_size, self.top + y * self.cell_size,
+                              self.cell_size, self.cell_size), 1)
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
+class Minesweeper(Board):
+    def __init__(self, w, h):
+        super().__init__(w, h)
+        for _ in range(10):
+            x, y = randint(0, self.w - 1), randint(0, self.h - 1)
+            self.board[y][x] = 10
+
+    def place(self, x, y):
+        if not self.on_board(x, y) or self.board[y][x] == 10:
+            return
+        self.board[y][x] = self.mines_count(x, y)
+
+    def mines_count(self, x, y):
+        directions = (-1, 0), (0, -1), (0, 1), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)
+        return len([(x + dx, y + dy) for dx, dy in directions
+                    if self.on_board(x + dx, y + dy) and self.board[y + dy][x + dx] == 10])
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.excepthook = except_hook
-    sys.exit(app.exec())
+pg.init()
+
+pg.display.set_caption('Дедушка сапёра')
+screen = pg.display.set_mode((320, 470))
+clock = pg.time.Clock()
+run = True
+FPS = 60
+
+minesweeper = Minesweeper(10, 15)
+
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_x, mouse_y = event.pos
+                x, y = ((mouse_x - minesweeper.left) // minesweeper.cell_size,
+                        (mouse_y - minesweeper.top) // minesweeper.cell_size)
+                minesweeper.place(x, y)
+
+    minesweeper.draw()
+    pg.display.flip()
+    clock.tick(FPS)
