746325f73ac858ef1a1df6b511ec10d621b1b39a
Goldman <goldmanmine@mail.ru>
2022-12-16 15:15:10 +0300

    Десант

--- main.py
+++ main.py
@@ -1,82 +1,55 @@
-import pygame as pg
-from random import randint
+import pygame
 
 
-class Board:
-    def __init__(self, w, h):
-        self.w, self.h = w, h
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
-        self.board = [[-1] * w for _ in range(h)]
-
-    def on_board(self, x, y):
-        return self.w - 1 >= x >= 0 <= y <= self.h - 1
-
-    def draw(self):
-        screen.fill('black')
-        font = pg.font.Font(None, 20)
-        for y in range(self.h):
-            for x in range(self.w):
-                pos = self.board[y][x]
-                if pos == 10:
-                    pg.draw.rect(screen, 'red',
-                                 (self.left + x * self.cell_size, self.top + y * self.cell_size,
-                                  self.cell_size, self.cell_size))
-                elif pos > -1:
-                    text = font.render(f'{pos}', True, (0, 255, 0))
-                    screen.blit(text, (self.left + 2 + x * self.cell_size, self.top + 2 + y * self.cell_size))
-                pg.draw.rect(screen, 'white',
-                             (self.left + x * self.cell_size, self.top + y * self.cell_size,
-                              self.cell_size, self.cell_size), 1)
+pygame.init()
+width, height = 729, 300
 
 
-class Minesweeper(Board):
-    def __init__(self, w, h):
-        super().__init__(w, h)
-        for _ in range(10):
-            x, y = randint(0, self.w - 1), randint(0, self.h - 1)
-            self.board[y][x] = 10
+class Mountain(pygame.sprite.Sprite):
+    image = pygame.image.load('mountains.png')
 
-    def open_cell(self, x, y):
-        if not self.on_board(x, y) or self.board[y][x] == 10:
-            return
-        mines = self.mines_count(x, y)
-        pos = self.board[y][x]
-        self.board[y][x] = mines
-        if not mines and pos != 0:
-            directions = (-1, 0), (0, -1), (0, 1), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)
-            for dx, dy in directions:
-                if self.on_board(x + dx, y + dy):
-                    self.open_cell(x + dx, y + dy)
-
-    def mines_count(self, x, y):
-        directions = (-1, 0), (0, -1), (0, 1), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)
-        return len([(x + dx, y + dy) for dx, dy in directions
-                    if self.on_board(x + dx, y + dy) and self.board[y + dy][x + dx] == 10])
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = height
 
 
-pg.init()
+class Landing(pygame.sprite.Sprite):
+    image = pygame.image.load("pt.png")
 
-pg.display.set_caption('Папа сапёра')
-screen = pg.display.set_mode((320, 470))
-clock = pg.time.Clock()
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self):
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(0, 1)
+
+
+all_sprites = pygame.sprite.Group()
+mountain = Mountain()
+
+pygame.display.set_caption('Высадка десанта')
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
 run = True
 FPS = 60
 
-minesweeper = Minesweeper(10, 15)
-
 while run:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    screen.fill('black')
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             run = False
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_x, mouse_y = event.pos
-                x, y = ((mouse_x - minesweeper.left) // minesweeper.cell_size,
-                        (mouse_y - minesweeper.top) // minesweeper.cell_size)
-                minesweeper.open_cell(x, y)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            Landing(event.pos)
 
-    minesweeper.draw()
-    pg.display.flip()
+    all_sprites.draw(screen)
+    all_sprites.update()
+    pygame.display.flip()
     clock.tick(FPS)
