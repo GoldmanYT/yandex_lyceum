837367e4cc3cb2b4b29cd5cca0a967006d190aed
Goldman <goldmanmine@mail.ru>
2023-01-05 13:59:01 +0300

    Убегающая кнопка

--- main3.py
+++ main3.py
@@ -1,8 +1,7 @@
 import sys
+from random import randint
 
 from PyQt5 import uic
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -10,23 +9,24 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.ufo_pixmap = QPixmap('ufo.png')
-        self.ufo.setPixmap(self.ufo_pixmap)
-        self.w, self.h = self.width() - self.ufo_pixmap.width(), self.height() - self.ufo_pixmap.height()
+        self.setMouseTracking(True)
+        self.w, self.h = self.width() - self.btn.width(), self.height() - self.btn.height()
 
-    def keyPressEvent(self, event):
-        x, y = self.ufo.x(), self.ufo.y()
-        if event.key() == Qt.Key_Up:
-            y -= 10
-        if event.key() == Qt.Key_Down:
-            y += 10
-        if event.key() == Qt.Key_Left:
-            x -= 10
-        if event.key() == Qt.Key_Right:
-            x += 10
-        x %= self.w
-        y %= self.h
-        self.ufo.move(x, y)
+    def mouseMoveEvent(self, event):
+        x, y = event.x(), event.y()
+        x0, y0 = self.btn.x(), self.btn.y()
+        x1, y1 = x0 + self.btn.width(), y0 + self.btn.height()
+        if x0 <= x <= x1 and y0 <= y <= y1:
+            self.move_btn(x, y)
+
+    def move_btn(self, x, y):
+        correct = False
+        w, h = self.btn.width(), self.btn.height()
+        while not correct:
+            x0, y0 = randint(0, 500 - w), randint(0, 500 - h)
+            if not (x0 <= x <= x0 + w and y0 <= y <= y0 + h):
+                correct = True
+        self.btn.move(x0, y0)
 
 
 def except_hook(cls, exception, traceback):
