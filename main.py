2cd8096b9451677a02f22c5ce7b36c6ee9fb9002
Goldman <goldmanmine@mail.ru>
2023-02-26 11:32:00 +0300

    Платформы

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import pygame as pg
 
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, x, y, *group):
-        super().__init__(*group)
+    def __init__(self, x, y):
+        for sprite in all_sprites:
+            if isinstance(sprite, Player):
+                all_sprites.remove(sprite)
+        for player in player_group:
+            player_group.remove(player)
+        super().__init__(all_sprites, player_group)
         self.image = pg.Surface((20, 20), pg.SRCALPHA)
         pg.draw.rect(self.image, 'blue', (0, 0, 20, 20))
         self.rect = pg.Rect((x, y, 20, 20))
@@ -20,8 +25,8 @@ class Player(pg.sprite.Sprite):
 
 
 class Platform(pg.sprite.Sprite):
-    def __init__(self, x, y, *group):
-        super().__init__(*group)
+    def __init__(self, x, y):
+        super().__init__(all_sprites, platform_group)
         self.image = pg.Surface((50, 10), pg.SRCALPHA)
         pg.draw.rect(self.image, 'gray', (0, 0, 50, 10))
         self.rect = pg.Rect(x, y, 50, 10)
@@ -30,6 +35,8 @@ class Platform(pg.sprite.Sprite):
 pg.init()
 
 all_sprites = pg.sprite.Group()
+player_group = pg.sprite.Group()
+platform_group = pg.sprite.Group()
 
 pg.display.set_caption('Платформы')
 screen = pg.display.set_mode((500, 500))
@@ -45,18 +52,16 @@ while run:
             run = False
         elif event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
-                Platform(*event.pos, all_sprites)
+                Platform(*event.pos)
             elif event.button == 3:
-                player = Player(*event.pos, all_sprites)
+                player = Player(*event.pos)
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_LEFT:
-                for sprite in all_sprites:
-                    if isinstance(sprite, Player):
-                        sprite.move(-10, 0)
+                for player in player_group:
+                    player.move(-10, 0)
             elif event.key == pg.K_RIGHT:
-                for sprite in all_sprites:
-                    if isinstance(sprite, Player):
-                        sprite.move(10, 0)
+                for player in player_group:
+                    player.move(10, 0)
 
     screen.fill('black')
 
