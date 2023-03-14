f65a026ceba344ae86d1f1841f0a5de140e6cfcd
Goldman <goldmanmine@mail.ru>
2023-03-14 08:00:36 +0300

    Настраиваемые часы с кукушкой

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import datetime
 import schedule
 
 
-def koo():
+def koo(hours, message='Ку'):
     hour = datetime.datetime.now().hour
-    print('Ку' * ((hour - 1) % 12 + 1))
+    if hour in hours:
+        print(message * ((hour - 1) % 12 + 1))
 
 
-schedule.every().hour.at('00:00').do(koo)
+a = list(range(24))
+message = input('Введите сообщение: ')
+hours = input('Введите диапазон: ')
+x, y = map(int, hours.split('-'))
+h = x
+while True:
+    a.remove(x)
+    if x == y:
+        break
+    x = (x + 1) % 24
+schedule.every().hour.at('00:00').do(koo, message=message, hours=a)
 
 while True:
     schedule.run_pending()
