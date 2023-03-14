8b9e097941c06ebb11c86b85110bbe46f022e60b
Goldman <goldmanmine@mail.ru>
2023-03-14 17:43:24 +0300

    Слайд-шоу

--- api-pygame.py
+++ api-pygame.py
@@ -1,17 +1,45 @@
 import requests
-import pygame
+import pygame as pg
+from sys import exit
 
-url = 'https://static-maps.yandex.ru/1.x/?l=map&z=10&pl=29.913396,59.891665,30.221951,59.918606,30.259094,59.917800,30.282579,59.933602,30.314566,59.939864'
+coords = [
+    '64.796296,54.468172',
+    '-0.554497,51.848595',
+    '-63.984938,-33.867512'
+]
+d = {
+    0: 16,
+    1: 17,
+    2: 16,
+}
 
-response = requests.get(url)
-if response:
-    with open('res.png', 'wb') as f:
-        f.write(response.content)
+for i, coord in enumerate(coords):
+    url = f'https://static-maps.yandex.ru/1.x/?l=sat&z={d[i]}&ll={coord}'
+    response = requests.get(url)
+    if response:
+        with open(f'res{i}.png', 'wb') as f:
+            f.write(response.content)
+    else:
+        print(response)
 
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load('res.png'), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+pg.init()
+screen = pg.display.set_mode((600, 450))
+pg.display.flip()
+run = True
+i = 0
+
+while run:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+            pg.quit()
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                i = (i - 1) % len(coords)
+            elif event.key == pg.K_RIGHT:
+                i = (i + 1) % len(coords)
+            else:
+                i = (i + 1) % len(coords)
+
+    screen.blit(pg.image.load(f'res{i}.png'), (0, 0))
+    pg.display.flip()
