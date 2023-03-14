92b777c6dce031ef904a07740f962099f3404d7b
Goldman <goldmanmine@mail.ru>
2023-03-14 20:22:40 +0300

    Форматирование файла

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--per-day', type=float, default=0)
-parser.add_argument('--per-week', type=float, default=0)
-parser.add_argument('--per-month', type=float, default=0)
-parser.add_argument('--per-year', type=float, default=0)
-parser.add_argument('--get-by', choices=['day', 'month', 'year'], type=str, default='day')
-args = parser.parse_args()
 
-if args.get_by == 'day':
-    print(int(args.per_day + args.per_week / 7 + args.per_month / 30 + args.per_year / 360))
-elif args.get_by == 'month':
-    print(int(args.per_day * 30 + args.per_week * 30 / 7 + args.per_month + args.per_year / 12))
-elif args.get_by == 'year':
-    print(int(args.per_day * 360 + args.per_week * 360 / 7 + args.per_month * 12 + args.per_year))
+def format_text_block(frame_height, frame_width, file_name):
+    try:
+        with open(file_name) as f:
+            data = f.read()
+    except Exception as e:
+        return e
+
+    res = ['']
+    for i in data:
+        if i == '\n':
+            if len(res) == frame_height:
+                break
+            else:
+                res.append('')
+        else:
+            if len(res[-1]) < frame_width:
+                res[-1] += i
+            else:
+                if len(res) == frame_height:
+                    break
+                else:
+                    res.append(i)
+
+    return '\n'.join(res)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--frame-height', type=int)
+    parser.add_argument('--frame-width', type=int)
+    parser.add_argument('file_name', type=str)
+    args = parser.parse_args()
+    print(format_text_block(args.frame_height, args.frame_width, args.file_name))
